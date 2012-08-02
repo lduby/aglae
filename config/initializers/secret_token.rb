df2b034e18f8f5c3b586dd2f33b1f01b47f7c0ef
Laetitia DUBY <laetitia.duby@gmail.com>
2012-08-02 14:48:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aglae::Application.config.secret_token = '800bfbb745cf99c911c58a38b0427bb200d3dd76c2a531a1c7f3be0a7f660673dd49078f64de471f01f0450440e5c586b25c84c85bfb5a92443d5a2bc3c830c1'

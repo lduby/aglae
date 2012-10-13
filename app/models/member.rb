402d245a6ec42250697f40778dafa450f12c6306
Laetitia DUBY <laetitia.duby@gmail.com>
2012-10-13 14:58:23 +0200

    User account dropdown displaying name and icon-user

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,4 +4,8 @@ class Member < ActiveRecord::Base
   has_many :children
   has_many :borrowings
   attr_accessible :address, :birthdate, :city, :firstname, :lastname, :occupation, :organization, :phone, :member_type, :zipcode
+  
+  def name
+    "#{firstname} #{lastname}"
+  end
 end

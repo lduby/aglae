86ff6be69b1b86be19f1385c35baa0c5e66e0b01
Laetitia DUBY <laetitia.duby@gmail.com>
2013-02-05 14:53:57 +0100

    I18n config

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,15 @@ module Aglae
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.time_zone = 'Paris'
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :fr
+    config.i18n.available_locales = [:fr, :en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
